511187da604455dfa21e774f9e03c0370bbdeb15
Sheikhbisma <bismasheikh2006@gmail.com>
2025-10-16 12:19:06 +0500

    committttt

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let video = document.getElementById("video");
 let icon = document.querySelector(".play-icon");
-window.onload = function() {
-  video.muted = false;
-};video.addEventListener("click", () => {
+let comment = document.querySelector(".comment-box");
+let com = document.querySelector("#come");
+let commentBox = document.querySelector(".commentBox");
+let closeBtn = document.querySelector(".closeBtn");
+let addBtn = document.getElementById("add");
+let input = document.getElementById("comments");
+
+
+video.addEventListener("click", () => {
   if (video.paused) {
     video.play();
     icon.style.opacity = "0";
@@ -11,4 +17,38 @@ window.onload = function() {
     video.pause();
     icon.style.opacity = "1";
   }
+});
+commentBox.addEventListener("click" ,()=>{
+  comment.classList.toggle("active");
+});
+input.addEventListener("input" , ()=>{
+  if(input.value.trim() !==""){
+    addBtn.classList.add("active");
+    addBtn.disabled=false;
+  }else{
+    addBtn.classList.remove("active");
+    addBtn.disabled = true;
+  }
+});
+addBtn.addEventListener("click" , ()=>{
+  let value = input.value
+  if(value.trim() !== ""){
+    let para = document.createElement("p");
+    para.innerHTML = `  <div class="img">
+                        <div class="first">
+                            <img src="/assets/profile-circle.svg" alt="">
+                            <b>You</b>
+                        </div>
+                        <div class="second">
+                            <p>${value}</p>
+                        </div>
+                       </div>`;
+    com.appendChild(para);
+    input.value = "";
+    addBtn.classList.remove("active");
+  addBtn.disabled = true;
+  }
+});
+closeBtn.addEventListener("click",()=>{
+  comment.classList.remove("active");
 });
