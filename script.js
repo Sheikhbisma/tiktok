20e24a7740099c748cd6f053b28aca59c261612c
Sheikhbisma <bismasheikh2006@gmail.com>
2025-10-17 06:57:25 +0500

    final commit

--- script.js
+++ script.js
@@ -1,30 +1,65 @@
 let video = document.getElementById("video");
-let icon = document.querySelector(".play-icon");
+let icons = document.querySelector(".play-icon");
 let comment = document.querySelector(".comment-box");
 let com = document.querySelector("#come");
 let commentBox = document.querySelector(".commentBox");
 let closeBtn = document.querySelector(".closeBtn");
 let addBtn = document.getElementById("add");
 let input = document.getElementById("comments");
+let counter = document.querySelector("#count");
+let icon = document.querySelector(".heart");
+let likeBtn = document.querySelector(".likeBtn");
+let footer = document.querySelector(".icons");
+let foot = document.querySelector(".footer");
+isLike = false;
+
 
 
 video.addEventListener("click", () => {
   if (video.paused) {
     video.play();
-    icon.style.opacity = "0";
+    icons.style.opacity = "0";
     video.muted=false;
 } else {
     video.pause();
-    icon.style.opacity = "1";
+    icons.style.opacity = "1";
   }
 });
+
+// like button logic
+likeBtn.addEventListener("click", () => {
+    if (!isLike) {
+        likeBtn.style.color = "red";
+        isLike = true;
+        console.log(isLike);
+      } else {
+      likeBtn.style.color = "";
+        isLike = false;
+       
+
+
+    }
+});
+video.addEventListener("dblclick", () => {
+  console.log("hello");
+  icon.classList.add("active");
+          likeBtn.style.color = "red";
+
+  setTimeout(() => {
+    icon.classList.remove("active");
+  }, 600);
+});
+
 commentBox.addEventListener("click" ,()=>{
   comment.classList.toggle("active");
+ foot.style.display = "none";
+
 });
 input.addEventListener("input" , ()=>{
   if(input.value.trim() !==""){
     addBtn.classList.add("active");
     addBtn.disabled=false;
+
   }else{
     addBtn.classList.remove("active");
     addBtn.disabled = true;
@@ -47,6 +82,7 @@ addBtn.addEventListener("click" , ()=>{
     input.value = "";
     addBtn.classList.remove("active");
   addBtn.disabled = true;
+
   }
 });
 closeBtn.addEventListener("click",()=>{
